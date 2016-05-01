3e78cff52b4184c2772bdd2b5fa45c7f1bfc7432
Ryan Yogan <ryanyogan@gmail.com>
2016-05-01 12:40:43 -0500

    setup board actions, wrapper view, reducers, api calls

---
+++ web/static/js/components/boards/form.js
@@ -0,0 +1,57 @@
+import React, { Component, PropTypes } from 'react';
+import PageClick from 'react-page-click';
+import Actions from '../../actions/boards';
+import { renderErrorsFor } from '../../utils';
+
+class BoardForm extends Component {
+  componentDidMount() {
+    this.refs.name.focus();
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+
+    const { dispatch } = this.props;
+    const { name } = this.refs;
+
+    const data = {
+      name: name.value,
+    };
+
+    dispatch(Actions.create(data));
+  }
+
+  _handleCancelClick(e) {
+    e.preventDefault();
+
+    this.props.onCancelClick();
+  }
+
+  render() {
+    const { errors } = this.props;
+
+    return (
+      <PageClick onClick={::this._handleCancelClick}>
+        <div className="board form">
+          <div className="inner">
+            <h4>New board</h4>
+            <form id="new_board_form" onSubmit={::this._handleSubmit}>
+              <input
+                ref="name"
+                id="board_name"
+                type="text"
+                placeholder="Board name"
+                required={true}
+              />
+              {renderErrorsFor(errors, 'name')}
+              <button type="submit">Create board</button> or
+              <a href="#" onClick={::this._handleCancelClick}>cancel</a>
+            </form>
+          </div>
+        </div>
+      </PageClick>
+    );
+  }
+}
+
+export default BoardForm;
