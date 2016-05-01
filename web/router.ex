20bc17f3ce8638cd8b22071d26411c810d95e13b
Ryan Yogan <ryanyogan@gmail.com>
2016-04-30 19:39:02 -0500

    configured API v1 route scoping

--- web/router.ex
+++ web/router.ex
@@ -9,6 +9,18 @@ defmodule SimpleScrum.Router do
     plug :put_secure_browser_headers
   end
 
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", SimpleScrum do
+    pipe_through :api
+
+    scope "/v1" do
+      post "/registration", RegistrationController, :create
+    end
+  end
+
   scope "/", SimpleScrum do
     pipe_through :browser # Use the default browser stack
 
