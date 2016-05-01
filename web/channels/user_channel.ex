76627c6923bb8f492b2ddbe22b5fe4ac8dc57343
Ryan Yogan <ryanyogan@gmail.com>
2016-05-01 11:47:34 -0500

    completed user socket / channel configuration

---
+++ web/channels/user_channel.ex
@@ -0,0 +1,7 @@
+defmodule SimpleScrum.UserChannel do
+  use SimpleScrum.Web, :channel
+
+  def join("users:" <> user_id, _params, socket) do
+    {:ok, socket}
+  end
+end
--- web/static/js/reducers/session.js
+++ web/static/js/reducers/session.js
@@ -4,6 +4,7 @@ import Constants from '../constants';
 const initialState = {
   currentUser: null,
   socket: null,
+  channel: null,
   error: null,
 };
 
@@ -12,11 +13,14 @@ export default function reducer(state = initialState, action = {}) {
     case Constants.CURRENT_USER:
       return { ...state, currentUser: action.currentUser, error: null };
 
-    case Constants.SESSIONS_ERROR:
-      return { ...state, error: action.error };
-
     case Constants.USER_SIGNED_OUT:
       return initialState;
+
+    case Constants.SOCKET_CONNECTED:
+      return { ...state, socket: action.socket, channel: action.channel };
+
+    case Constants.SESSIONS_ERROR:
+      return { ...state, error: action.error };
 
     default:
       return state;
